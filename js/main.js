7db9324f8861aeac36ea6eb3c88720c9a30698b2
vbnm7180 <vbnm7180@mail.ru>
2020-07-08 18:31:25 +0400

    master

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.footer__about-btn').click(function() {
+    $('.footer__links1').slideToggle();
+
+});
+
+$('.footer__help-btn').click(function() {
+    $('.footer__links2').slideToggle();
+
+});
